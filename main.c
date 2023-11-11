6fb834d824e0fdc9368245d69f61bd28cc09dacb
princz-1 <150322497+princz-1@users.noreply.github.com>
2023-11-11 06:27:28 +0000

    added code
    basic code

--- main.c
+++ main.c
@@ -6,6 +6,23 @@ int main(void) {
   int *ptr = &age;
   int _age = *ptr;
 
-  printf("%d", _age);
+
+  
+  //value print
+  printf("%d\n", age);
+  printf("%d\n",*ptr);
+  printf("%d\n",*(&age));
+
+
+  
+  //Address print (Hexadecimal number print)
+  printf("%p\n",&age);
+  //unsigned number print (integer number print)
+  printf("%u\n",&age);
+  //print ptr
+  printf("%u\n",ptr);
+  //print address of ptr
+  printf("%u\n",&ptr);
+  
   return 0;
 }
